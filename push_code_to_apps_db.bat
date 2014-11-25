f8ebeaa4add69182a396f126630e16b339c3b468
omeryousaf <u.yusaf@gmail.com>
2014-11-25 12:52:43 +0500

    fixed the node_module missing issue that was failing script to push app. also updated a design doc

--- push_code_to_apps_db.bat
+++ push_code_to_apps_db.bat
@@ -1,1 +1,3 @@
-BeLL-Apps\node_modules\.bin\couchapp push BeLL-Apps\app.js http://127.0.0.1:5984/apps
+cd BeLL-Apps
+call npm install
+node_modules\.bin\couchapp push app.js http://127.0.0.1:5984/apps
