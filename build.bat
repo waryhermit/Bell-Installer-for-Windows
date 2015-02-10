7efba8d0c022b9e783372f60516820c5ea5e185b
Adnan Khalil <mr.adnaan004@gmail.com>
2015-02-10 20:00:46 +0500

    Updated release tag in Build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 7z920.exe
-curl -LOk https://github.com/open-learning-exchange/BeLL-Apps/archive/v0.11.19.zip
-7z.exe x v0.11.19.zip
+curl -LOk https://github.com/open-learning-exchange/BeLL-Apps/archive/v0.11.24.zip
+7z.exe x v0.11.24.zip
 timeout 2
-move BeLL-Apps-0.11.19 BeLL-Apps
-del /f v0.11.19.zip
+move BeLL-Apps-0.11.24 BeLL-Apps
+del /f v0.11.24.zip
