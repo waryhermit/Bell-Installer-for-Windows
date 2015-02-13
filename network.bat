7d45fcde0c2c73c361ea564a20acd1fc77a060fb
dogi <stefan@unterhauser.name>
2015-02-13 11:41:44 -0500

    firewall tweaks and global couchdb config added

--- network.bat
+++ network.bat
@@ -1,3 +1,11 @@
 "%~dp0\Executables\BonjourPSSetup.exe"
 timeout 2
+netsh advfirewall firewall add rule name="CouchDB/HTTP" dir=out action=allow protocol=TCP localport=5984
 timeout 2
+netsh advfirewall firewall add rule name="CouchDB/HTTP" dir=in action=allow protocol=TCP localport=5984
+timeout 2
+netsh advfirewall firewall add rule name="CouchDB/HTTPS" dir=out action=allow protocol=TCP localport=6984
+timeout 2
+netsh advfirewall firewall add rule name="CouchDB/HTTPS" dir=in action=allow protocol=TCP localport=6984
+timeout 2
+(Get-Content "C:\Program Files (x86)\Apache Software Foundation\CouchDB\etc\couchdb\default.ini") | Foreach-Object {$_ -replace '127.0.0.1','0.0.0.0'}  | Out-File "C:\Program Files (x86)\Apache Software Foundation\CouchDB\etc\couchdb\default.ini"
