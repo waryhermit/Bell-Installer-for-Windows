5251b00572ee8fd54b08361c003358a186998253
Adnan Khalil <mr.adnaan004@gmail.com>
2015-03-18 19:03:30 +0500

    Updated Build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 7z920.exe
-curl -LOk https://github.com/open-learning-exchange/BeLL-Apps/archive/0.11.32.zip
-7z.exe x 0.11.32.zip
+curl -LOk https://github.com/open-learning-exchange/BeLL-Apps/archive/0.11.33.zip
+7z.exe x 0.11.33.zip
 timeout 15
-move BeLL-Apps-0.11.32 BeLL-Apps
-del /f 0.11.32.zip
+move BeLL-Apps-0.11.33 BeLL-Apps
+del /f 0.11.33.zip
