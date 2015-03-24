bcf421c2973fdeb7103716015d9c2094834bc017
Adnan Khalil <mr.adnaan004@gmail.com>
2015-03-24 16:05:16 +0500

    Bell-Apps removed (added by mistake)

--- BeLL-Apps/Update_Bell-Apps_to_Current.bat
+++
@@ -1,2 +0,0 @@
-npm install
-node install_windows http://127.0.0.1:5984
--- BeLL-Apps/update_nation/update_nation_app.bat
+++
@@ -1,3 +0,0 @@
-call install_node_modules.bat
-node push_nation_design_docs.js %1
-curl -X PUT %1/_config/httpd/allow_jsonp -d \"true\"
