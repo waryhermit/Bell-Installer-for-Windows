c96532699521a22ae3be7a4fdf7877f46fb1c4e0
Adnan Khalil <mr.adnaan004@gmail.com>
2015-03-27 11:45:34 +0500

    Integrated Starter Data

--- install.bat
+++ install.bat
@@ -1,4 +1,4 @@
-@echo off
+::@echo off
 
 :: run node.js executable/installer wizard
 Executables\node-v0.10.26-x86.msi
@@ -21,83 +21,50 @@ cmd /c .\Executables\startcouchdb.bat
 curl -X PUT http://localhost:5984/_config/httpd/bind_address -d "\"0.0.0.0\""
 
 :: delete databases
-curl -X DELETE http://localhost:5984/activitylog
-curl -X DELETE http://localhost:5984/apps
-curl -X DELETE http://localhost:5984/assignmentpaper
-curl -X DELETE http://localhost:5984/assignments
-curl -X DELETE http://localhost:5984/calendar
-curl -X DELETE http://localhost:5984/collectionlist
-curl -X DELETE http://localhost:5984/communities
-curl -X DELETE http://localhost:5984/community
-curl -X DELETE http://localhost:5984/communityreports
-curl -X DELETE http://localhost:5984/configurations
-curl -X DELETE http://localhost:5984/courseschedule
-curl -X DELETE http://localhost:5984/coursestep
-curl -X DELETE http://localhost:5984/feedback
-curl -X DELETE http://localhost:5984/groups
-curl -X DELETE http://localhost:5984/invitations
-curl -X DELETE http://localhost:5984/languages
-curl -X DELETE http://localhost:5984/mail
-curl -X DELETE http://localhost:5984/meetups
-curl -X DELETE http://localhost:5984/members
-curl -X DELETE http://localhost:5984/membercourseprogress
-curl -X DELETE http://localhost:5984/nationreports
-curl -X DELETE http://localhost:5984/publicationdistribution
-curl -X DELETE http://localhost:5984/publications
-curl -X DELETE http://localhost:5984/report
-curl -X DELETE http://localhost:5984/requests
-curl -X DELETE http://localhost:5984/resourcefrequency
-curl -X DELETE http://localhost:5984/resources
-curl -X DELETE http://localhost:5984/shelf
-curl -X DELETE http://localhost:5984/usermeetups
+FOR /R BeLL-Apps\databases %%F in (*.*) do (
+    curl -X DELETE http://localhost:5984/%%~nF
+)
 
 :: create databases
-curl -X PUT http://localhost:5984/activitylog
-curl -X PUT http://localhost:5984/apps
-curl -X PUT http://localhost:5984/assignmentpaper
-curl -X PUT http://localhost:5984/assignments
-curl -X PUT http://localhost:5984/calendar
-curl -X PUT http://localhost:5984/collectionlist
-curl -X PUT http://localhost:5984/communities
-curl -X PUT http://localhost:5984/community
-curl -X PUT http://localhost:5984/communityreports
-curl -X PUT http://localhost:5984/configurations
-curl -X PUT http://localhost:5984/courseschedule
-curl -X PUT http://localhost:5984/coursestep
-curl -X PUT http://localhost:5984/feedback
-curl -X PUT http://localhost:5984/groups
-curl -X PUT http://localhost:5984/invitations
-curl -X PUT http://localhost:5984/languages
-curl -X PUT http://localhost:5984/mail
-curl -X PUT http://localhost:5984/meetups
-curl -X PUT http://localhost:5984/members
-curl -X PUT http://localhost:5984/membercourseprogress
-curl -X PUT http://localhost:5984/nationreports
-curl -X PUT http://localhost:5984/publicationdistribution
-curl -X PUT http://localhost:5984/publications
-curl -X PUT http://localhost:5984/report
-curl -X PUT http://localhost:5984/requests
-curl -X PUT http://localhost:5984/resourcefrequency
-curl -X PUT http://localhost:5984/resources
-curl -X PUT http://localhost:5984/shelf
-curl -X PUT http://localhost:5984/usermeetups
+FOR /R BeLL-Apps\databases %%F in (*.*) do (
+    curl -X PUT http://localhost:5984/%%~nF
+)
 
 :: add bare minimal required data to couchdb for launching bell-apps smoothly
 curl -d @BeLL-Apps\init_docs\languages.txt -H "Content-Type: application/json" -X POST http://localhost:5984/languages
 curl -d @BeLL-Apps\init_docs\ConfigurationsDoc-Community.txt -H "Content-Type: application/json" -X POST http://localhost:5984/configurations
 curl -d @BeLL-Apps\init_docs\admin.txt -H "Content-Type: application/json" -X POST http://localhost:5984/members
 
+:: delete empty dbs we want to replace with Starter data dbs
+curl -X DELETE http://localhost:5984/collectionlist
+curl -X DELETE http://localhost:5984/resources
+curl -X DELETE http://localhost:5984/groups
+curl -X DELETE http://localhost:5984/coursestep
+
+set couchdb_databases_folder="C:\Program Files (x86)\Apache Software Foundation\CouchDB\var\lib\couchdb\"
+set starter_data_folder="StarterData\"
+
+:: copy the resources, collectionlist, groups, and coursestep db files into the couchdb_databases_folder
+copy /y %starter_data_folder%resources.couch %couchdb_databases_folder%resources.couch
+copy /y %starter_data_folder%collectionlist.couch %couchdb_databases_folder%collectionlist.couch
+copy /y %starter_data_folder%groups.couch %couchdb_databases_folder%groups.couch
+copy /y %starter_data_folder%coursestep.couch %couchdb_databases_folder%coursestep.couch
+
+:: if "Starter_Data" folder did not have any data in it, then we need to create the databases again
+curl -X PUT http://localhost:5984/collectionlist
+curl -X PUT http://localhost:5984/coursestep
+curl -X PUT http://localhost:5984/groups
+curl -X PUT http://localhost:5984/resources
+
 SET PATH=%PATH%;C:\Users\%USERNAME%\AppData\Roaming\npm;C:\Program Files (x86)\nodejs\
 
 call .\install_nodemodules.bat
 
 :: add design docs to all databases
-cd BeLL-Apps
-FOR /R .\databases %%F in (*.*) do (
-call node_modules\.bin\couchapp push databases\%%~nF%%~xF http://localhost:5984/%%~nF
+FOR /R BeLL-Apps\databases %%F in (*.*) do (
+call BeLL-Apps\node_modules\.bin\couchapp push BeLL-Apps\databases\%%~nxF http://localhost:5984/%%~nF
 timeout 1
 )
-cd %~dp0
 
 start Launch_PDFOptimizer.bat
 
