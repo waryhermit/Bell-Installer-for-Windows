c481f29a75c40072f382ef37aa455fb40fb5092e
SadiaRasheed <sadia.rasheed@northbaysolutions.net>
2015-09-11 16:09:15 +0500

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 7z920.exe
-curl -LOk https://github.com/open-learning-exchange/BeLL-Apps/archive/0.11.58.zip
-7z.exe x 0.11.58.zip
+curl -LOk https://github.com/open-learning-exchange/BeLL-Apps/archive/0.11.59.zip
+7z.exe x 0.11.59.zip
 timeout 15
-move BeLL-Apps-0.11.58 BeLL-Apps
-del /f 0.11.58.zip
+move BeLL-Apps-0.11.59 BeLL-Apps
+del /f 0.11.59.zip
