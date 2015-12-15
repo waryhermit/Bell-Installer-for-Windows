893386dc9197711e8479b103aa02f13977adb0d6
MUHAMMAD WAQAS <muhammad.waqas@northbaysolutions.net>
2015-12-15 09:50:21 +0500

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 7z920.exe
-curl -LOk https://github.com/open-learning-exchange/BeLL-Apps/archive/0.11.82.zip
-7z.exe x 0.11.82.zip
+curl -LOk https://github.com/open-learning-exchange/BeLL-Apps/archive/0.11.83.zip
+7z.exe x 0.11.83.zip
 timeout 15
-move BeLL-Apps-0.11.82 BeLL-Apps
-del /f 0.11.82.zip
+move BeLL-Apps-0.11.83 BeLL-Apps
+del /f 0.11.83.zip
