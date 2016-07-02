c0b3a4eaf6dab2d0ac7991d5fc0a56e8abff7163
dogi <dogi@users.noreply.github.com>
2016-07-01 20:23:47 -0400

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 7z920.exe
-curl -LOk https://github.com/open-learning-exchange/BeLL-Apps/archive/0.12.26.zip
-7z.exe x 0.12.26.zip
+curl -LOk https://github.com/open-learning-exchange/BeLL-Apps/archive/0.12.27.zip
+7z.exe x 0.12.27.zip
 timeout 15
-move BeLL-Apps-0.12.26 BeLL-Apps
-del /f 0.12.26.zip
+move BeLL-Apps-0.12.27 BeLL-Apps
+del /f 0.12.27.zip
